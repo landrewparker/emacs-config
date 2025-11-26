487a9962e80927035d8c290ad8b381f1f427a827
Andrew Parker <anparker@nvidia.com>
2025-11-26 14:27:06 -0800

    Switch to package.el

--- init.el
+++ init.el
@@ -4,45 +4,9 @@
 
 ;;; Code:
 
-;; Bootstrap Elpaca
-(defvar elpaca-installer-version 0.11)
-(defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
-(defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
-(defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
-(defvar elpaca-order '(elpaca :repo "https://github.com/progfolio/elpaca.git"
-                              :ref nil :depth 1 :inherit ignore
-                              :files (:defaults "elpaca-test.el" (:exclude "extensions"))
-                              :build (:not elpaca--activate-package)))
-(let* ((repo  (expand-file-name "elpaca/" elpaca-repos-directory))
-       (build (expand-file-name "elpaca/" elpaca-builds-directory))
-       (order (cdr elpaca-order))
-       (default-directory repo))
-  (add-to-list 'load-path (if (file-exists-p build) build repo))
-  (unless (file-exists-p repo)
-    (make-directory repo t)
-    (when (<= emacs-major-version 28) (require 'subr-x))
-    (condition-case-unless-debug err
-        (if-let* ((buffer (pop-to-buffer-same-window "*elpaca-bootstrap*"))
-                  ((zerop (apply #'call-process `("git" nil ,buffer t "clone"
-                                                  ,@(when-let* ((depth (plist-get order :depth)))
-                                                      (list (format "--depth=%d" depth) "--no-single-branch"))
-                                                  ,(plist-get order :repo) ,repo))))
-                  ((zerop (call-process "git" nil buffer t "checkout"
-                                        (or (plist-get order :ref) "--"))))
-                  (emacs (concat invocation-directory invocation-name))
-                  ((zerop (call-process emacs nil buffer nil "-Q" "-L" "." "--batch"
-                                        "--eval" "(byte-recompile-directory \".\" 0 'force)")))
-                  ((require 'elpaca))
-                  ((elpaca-generate-autoloads "elpaca" repo)))
-            (progn (message "%s" (buffer-string)) (kill-buffer buffer))
-          (error "%s" (with-current-buffer buffer (buffer-string))))
-      ((error) (warn "%s" err) (delete-directory repo 'recursive))))
-  (unless (require 'elpaca-autoloads nil t)
-    (require 'elpaca)
-    (elpaca-generate-autoloads "elpaca" repo)
-    (let ((load-source-file-function nil)) (load "./elpaca-autoloads"))))
-(add-hook 'after-init-hook #'elpaca-process-queues)
-(elpaca `(,@elpaca-order))
+;; Configure package.el
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 
 ;; Declare use-package
 (eval-when-compile
@@ -50,14 +14,9 @@
 ;; (setq use-package-verbose t)
 ;; (setq use-package-compute-statistics t)
 
-;; Install elpaca use-package support and enable :ensure keyword
-(elpaca elpaca-use-package
-  (elpaca-use-package-mode))
-(elpaca-wait)
-
 ;; Make diminish available for use-package :diminish
 (use-package diminish
-  :ensure (:wait t))
+  :ensure t)
 
 ;; emacs
 (use-package emacs
@@ -188,7 +147,7 @@
 
 ;; casual
 (use-package casual
-  :ensure (:depth 1)
+  :ensure t
   :bind (:map calc-mode-map ("C-o" . casual-calc-tmenu)))
 
 ;; clipetty
@@ -398,7 +357,7 @@
 
 ;; modus-themes
 (use-package modus-themes
-  :ensure (:depth 1)
+  :ensure t
   :defer t)
 
 ;; nord-theme
@@ -407,7 +366,8 @@
 ;; https://github.com/arcticicestudio/nord-emacs/issues/79.
 ;;
 (use-package nord-theme
-  :ensure (:host github :repo "landrewparker/nord-emacs" :branch "main")
+  :ensure t
+  :vc (:url "https://github.com/landrewparker/nord-emacs.git")
   :defer t)
 
 ;; orderless
@@ -503,14 +463,8 @@
 
 ;; verilog-mode
 (use-package verilog-mode
+  :ensure t
   :defer t
-  :ensure (:host github
-           :repo "veripool/verilog-mode"
-           ;; The default make target also compiles e/verilog-mode.elc
-           ;; which will not be used, but seems better than the
-           ;; potentially fragile "e/verilog-mode.el" target.
-           :pre-build (("make"))
-           :files ("e/*.el"))
   :custom
   (verilog-auto-newline nil)
   (verilog-indent-level 2)
@@ -525,7 +479,7 @@
 
 ;; vscode-dark-plus-theme
 (use-package vscode-dark-plus-theme
-  :ensure (:depth 1)
+  :ensure t
   :defer t)
 
 ;; wgrep
